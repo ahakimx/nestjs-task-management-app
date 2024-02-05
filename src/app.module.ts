d934808cc35ff87029ac6697f421da351d2413bf
ahakimx <ahakim338@gmail.com>
2024-02-06 03:16:08 +0700

    create task entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
+      password: 'postgres',
       database: 'task-management',
       autoLoadEntities: true,
       synchronize: true,
