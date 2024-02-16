7765d21cfedcc0b20d719dcc79668057cb831a32
ahakimx <ahakim338@gmail.com>
2024-02-17 00:51:18 +0700

    deleting a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -57,10 +57,13 @@ export class TasksService {
 
     }
 
-    // deleteTask(id: string): void {
-    //     const found = this.getTaskById(id);
-    //     this.tasks = this.tasks.filter((task) => task.id !== found.id);
-    // }
+    async deleteTask(id: string): Promise<void> {
+        const result = await this.taskRepository.delete(id);
+        
+        if (result.affected === 0) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+    }
 
     // updateTaskStatus(id: string, status: TaskStatus) {
     //     const task = this.getTaskById(id);
