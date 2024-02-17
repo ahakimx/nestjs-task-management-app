4322a5d993ffe22e237955005f62c8384895752b
ahakimx <ahakim338@gmail.com>
2024-02-17 14:28:36 +0700

    adding jwt token on sign in

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,19 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'topSecR3t123',
+      signOptions: {
+        expiresIn: 3600,
+      },
+
+    }),
     TypeOrmModule.forFeature([User])
   ],
   providers: [AuthService, UserRepository],
